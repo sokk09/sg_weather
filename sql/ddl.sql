40bbaaceadb93ab100eaec0a1790c89efc78f0ee
sokk09 <sokyi1997@gmail.com>
2025-04-15 14:18:56 +0800

    add raw tables ddl

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,3 +1,21 @@
+CREATE TABLE raw_stations (
+    id VARCHAR,
+    deviceid VARCHAR,
+    name VARCHAR,
+    latitude NUMERIC,
+    longitude NUMERIC,
+    PRIMARY KEY(id)
+    )
+;
+
+CREATE TABLE raw_readings (
+    timestamp timestamp,
+    stationid VARCHAR REFERENCES stations (id),
+    value NUMERIC,
+    PRIMARY KEY (timestamp, stationId)
+    )
+;
+
 CREATE TABLE stations (
     id VARCHAR,
     deviceid VARCHAR,
